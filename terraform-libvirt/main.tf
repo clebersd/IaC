e323a71e8194f0aab47654e059559ac8c490d26e
Linux da Bahia <cleberreis17@gmail.com>
2026-07-09 17:37:35 -0300

    v2

---
+++ terraform-libvirt/main.tf
@@ -0,0 +1,34 @@
+
+terraform {
+  
+  required_providers {
+    
+    libvirt = {
+
+      source = "dmacvicar/libvirt"
+      version = "0.8.3"
+      
+    }
+
+  }
+
+}
+
+
+provider "libvirt" {
+  
+  uri =  "qemu:///system"
+
+}
+
+
+
+module "vms_libvirt" {
+  
+  source = "./hosts/"
+
+  _hostname = var._hostname
+ 
+
+}
+
---
+++ terraform-libvirt/variables.tf
@@ -0,0 +1,23 @@
+variable "_hostname" {
+    
+    type = string
+    description = ""
+
+}
+
+variable  "disk" {
+   type = list(string)
+   default = ["libvirt_volume.host0.id","libvirt_volume.host1.id","libvirt_volume.host2.id","libvirt_volume.host3.id"]
+}
+
+variable  "storage" {
+
+    default ="/home/reis/storage"
+}
+
+
+variable  "imag_debian" {
+
+    default = "https://cloud.debian.org/images/cloud/trixie/20260601-2496/debian-13-generic-amd64-20260601-2496.qcow2"
+
+}
